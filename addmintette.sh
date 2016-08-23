694cce2bc84f7832fa8909925a49d72f593f48aa
Mikhail Volkhov <volhovm.cs@gmail.com>
2016-08-23 22:23:03 +0300

    [RSC-133] Add generic nixops.nix parsing to scripts

--- addmintette.sh
+++ addmintette.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+source ./parseNix.sh
 echo "Getting host..."
 mintetteHost=$(nixops info -d rscoin | grep $1 | sed -e "s/.*|\s\([0-9\.]\+\)\s*|\s*/\1/")
 echo "Mintette $1 is on $mintetteHost, getting key"
-mintetteKey=$(nixops ssh $1 -- cat /var/lib/rscoin-mintette/.rscoin/mintette3100Key.pub)
+mintetteKey=$(nixops ssh $1 -- cat /var/lib/rscoin-mintette/.rscoin/mintette${NIXOPS_MINTETTE_PORT}Key.pub)
 echo "Has key $mintetteKey, now adding it to the bank"
-nixops ssh rs-bank -- rscoin-bank --log-severity Debug --secret-key /var/lib/rscoin-bank/.rscoin/bankKey --config-path \$RSCOIN_CONFIG add-mintette --key $mintetteKey --host $mintetteHost --port 3100
+nixops ssh rs-bank -- rscoin-bank --log-severity Debug --secret-key /var/lib/rscoin-bank/.rscoin/bankKey --config-path \$RSCOIN_CONFIG add-mintette --key $mintetteKey --host $mintetteHost --port $NIXOPS_MINTETTE_PORT
